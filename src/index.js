4936a5f782924a9b3abae3e080cc62ff86058e40
Meraki-Arcana <merakiarcana@gmail.com>
2024-09-13 19:14:20 -0600

    Implemented search functionality
    Users are able to search for a city using the search form.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function searchForm(event) {
+function displayWeatherData(response) {
+    let temperatureElement = document.querySelector("#temperature")
+    temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city-name");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML =  Math.round(temperature)
+}
+
+
+function searchingForCity(city) {
+//make API call and update the UI
+let apiKey = "97ebcf39f3b8605d5a644of06ft2846e";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(displayWeatherData);
+}
+
+function searchInquiry(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city-name");
-    cityElement.innerHTML = searchInput.value;
-    //call the API
+
+    searchingForCity(searchInput.value);
 }
 
 
 
 let searchformElement = document.querySelector("#search-form")
-searchformElement.addEventListener("submit", searchForm)
+searchformElement.addEventListener("submit", searchInquiry)
+
+searchingForCity("Monument")
