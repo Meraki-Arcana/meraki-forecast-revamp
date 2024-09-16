b47dcbe33b3cdfd5dd8f368f873dd7ca5bb4e1f6
Meraki-Arcana <merakiarcana@gmail.com>
2024-09-15 20:00:34 -0600

    Added more weather data
    I added the current date/time, humidity, and wind speeds for any location.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,68 @@
 function displayWeatherData(response) {
-    let temperatureElement = document.querySelector("#temperature")
-    temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city-name");
-    cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML =  Math.round(temperature)
+  let temperatureElement = document.querySelector("#temperature");
+  temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.city;
+
+  let conditionElement = document.querySelector("#temperature-condition");
+  conditionElement.innerHTML = response.data.condition.description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed)
+
+  currentTime = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  currentTime.innerHTML = formatDate(date)
 }
 
+function formatDate(date) {
+  let day = date.getDay();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+
+  return `${formattedDay} ${hours}:${minutes}`;
+}
 
 function searchingForCity(city) {
-//make API call and update the UI
-let apiKey = "97ebcf39f3b8605d5a644of06ft2846e";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-axios.get(apiUrl).then(displayWeatherData);
+  //make API call and update the UI
+  let apiKey = "97ebcf39f3b8605d5a644of06ft2846e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeatherData);
 }
 
 function searchInquiry(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
 
-    searchingForCity(searchInput.value);
+  searchingForCity(searchInput.value);
 }
 
+let searchformElement = document.querySelector("#search-form");
+searchformElement.addEventListener("submit", searchInquiry);
 
-
-let searchformElement = document.querySelector("#search-form")
-searchformElement.addEventListener("submit", searchInquiry)
-
-searchingForCity("Monument")
+searchingForCity("Monument");
