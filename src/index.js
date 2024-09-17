1ced76f6eba52cd046fcc4c1c9b6c74a175c1bcc
Meraki-Arcana <merakiarcana@gmail.com>
2024-09-17 10:54:33 -0600

    Created a 'displayForecast' function
    I terminated the redundancy of my forecast summary ( in HTML) and moved it into a newly created JavaScript function. The displayForecast function loops 5 times (representing each day of the week), and injects the previous HTML code.

--- src/index.js
+++ src/index.js
@@ -13,14 +13,14 @@ function displayWeatherData(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 
   let windSpeedElement = document.querySelector("#wind-speed");
-  windSpeedElement.innerHTML = Math.round(response.data.wind.speed)
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
 
   currentTime = document.querySelector("#current-time");
   let date = new Date(response.data.time * 1000);
-  currentTime.innerHTML = formatDate(date)
+  currentTime.innerHTML = formatDate(date);
 
-  let weatherIcon = document.querySelector("#weather-icon")
-  
+  let weatherIcon = document.querySelector("#weather-icon");
+
   weatherIcon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon">`;
 }
 
@@ -66,7 +66,34 @@ function searchInquiry(event) {
   searchingForCity(searchInput.value);
 }
 
+function displayForecast() {
+  
+  let days = ['Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
+  let forecastHtml = ""
+  
+  days.forEach(function(day) {
+    forecastHtml = forecastHtml +
+    `<div class="forecast-summary">
+    <div class="day-of-week">${day}</div>
+    <div class="forecast-icon">☁️</div>
+    <div class="forecast-temperatures">
+    <strong>
+    <div class="high-low-temp">71°</div>
+    </strong>
+    <div class="high-low-temp">66°</div>
+    </div>
+    </div>
+    `;
+  });
+  
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml
+}
+
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", searchInquiry);
 
 searchingForCity("Monument");
+displayForecast();
+
+
